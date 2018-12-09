0932d61808c4168cf30166a6ca61847b7e8aae91
DonovanDwyer <dwyer.donovan@gmail.com>
2018-12-09 09:54:26 -0500

    Done.

--- src/reducers/combineReducers.js
+++ src/reducers/combineReducers.js
@@ -32,16 +32,6 @@ function authorsReducer(state = [], action){
       idx = state.authors.indexOf(action.id);
       return [state.authors.slice(0, idx), state.authors.slice(idx + 1)];
 
-    case "ADD_BOOK":
-      let existingAuthor = state.filter(
-        author => author.authorName === action.book.authorName
-      );
-      if (existingAuthor.length > 0) {
-        return state;
-      } else {
-        return [...state, { authorName: action.book.authorName, id: uuid() }];
-      }
-
     default:
       return state;
     }
